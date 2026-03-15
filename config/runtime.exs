be7786710574f894fc098135edeb90d05511a05b
ZulKris <2garin.zmej@gmail.com>
2026-03-15 22:51:23 +0300

    fix Dockerfile target at fly.toml

--- config/runtime.exs
+++ config/runtime.exs
@@ -40,7 +40,7 @@ if config_env() == :prod do
       """
 
   host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("PORT") || "4000")
+  port = String.to_integer(System.get_env("PORT") || "8080")
 
   config :painting_crew, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
